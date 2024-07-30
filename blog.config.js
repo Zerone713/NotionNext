500a6961389edab8927804a883937298af5608c3
Zerone713 <59997124+Zerone713@users.noreply.github.com>
2024-07-30 09:50:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '零点', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个不正经的设计师🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-orpin-chi.vercel.app', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.zeroblogs.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，零点，暖通，二次开发，Python', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
